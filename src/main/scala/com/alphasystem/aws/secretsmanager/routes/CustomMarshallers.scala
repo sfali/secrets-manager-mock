ab41796d6f009142dd122858e3a9d0413fb80008
sali <sali@loyalty.com>
2019-08-14 17:35:14 -0400

    fix name

--- src/main/scala/com/alphasystem/aws/secretsmanager/routes/CustomMarshallers.scala
+++ src/main/scala/com/alphasystem/aws/secretsmanager/routes/CustomMarshallers.scala
@@ -2,7 +2,7 @@ package com.alphasystem.aws.secretsmanager.routes
 
 import akka.http.scaladsl.marshalling.Marshaller.fromToEntityMarshaller
 import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller, ToResponseMarshaller}
-import akka.http.scaladsl.model.{HttpEntity, StatusCodes}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import com.alphasystem.aws.secretsmanager.model.Errors
 import com.alphasystem.aws.secretsmanager.routes.model.CreateSecretResponse
 import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport
@@ -11,17 +11,16 @@ trait CustomMarshallers extends ErrorAccumulatingCirceSupport {
 
   import Errors._
   import StatusCodes._
-  import akka.http.scaladsl.model.ContentTypes._
 
-  private val defaultContentType = `application/json`
+  private val defaultContentType = ContentTypes.`application/json`
 
-  private def errorResponseMarshallers[T]: ToEntityMarshaller[ErrorResponse] =
+  private def errorResponseMarshaller[T]: ToEntityMarshaller[ErrorResponse] =
     Marshaller.withFixedContentType(defaultContentType) { result =>
       HttpEntity(defaultContentType, result.toJson)
     }
 
-  implicit val NoSuchBucketExceptionMarshallers: ToEntityMarshaller[ResourceExistsException] =
-    errorResponseMarshallers[ResourceExistsException]
+  implicit val ResourceExistsExceptionMarshaller: ToEntityMarshaller[ResourceExistsException] =
+    errorResponseMarshaller[ResourceExistsException]
 
   implicit val CreateSecretResponse: ToResponseMarshaller[CreateSecretResponse] =
     fromToEntityMarshaller[CreateSecretResponse](OK)
