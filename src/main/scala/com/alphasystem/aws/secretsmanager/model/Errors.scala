b09d610b653ec58047abbb823086f50bd0e72deb
sali <sali@loyalty.com>
2019-08-14 12:10:10 -0400

    new error

--- src/main/scala/com/alphasystem/aws/secretsmanager/model/Errors.scala
+++ src/main/scala/com/alphasystem/aws/secretsmanager/model/Errors.scala
@@ -2,6 +2,9 @@ package com.alphasystem.aws.secretsmanager.model
 
 object Errors {
 
+  case object InvalidRequestException
+    extends Exception("You provided a parameter value that is not valid for the current state of the resource.")
+
   case object ResourceExistsException
     extends Exception("A resource with the ID you requested already exists.")
 
