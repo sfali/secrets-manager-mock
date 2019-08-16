c82b3354769d52e8ed9a15c4be9f494d1b45ac64
sali <sali@loyalty.com>
2019-08-16 07:25:29 -0400

    added secrets manager targets

--- src/main/scala/com/alphasystem/aws/secretsmanager/model/Target.scala
+++ src/main/scala/com/alphasystem/aws/secretsmanager/model/Target.scala
@@ -9,12 +9,40 @@ sealed trait Target extends EnumEntry
 object Target extends Enum[Target] with CirceEnum[Target] {
   override def values: immutable.IndexedSeq[Target] = findValues
 
+  case object CancelRotateSecret extends Target
+
   case object CreateSecret extends Target
+
+  case object DeleteResourcePolicy extends Target
+
+  case object DescribeSecret extends Target
+
+  case object DeleteSecret extends Target
+
+  case object GetRandomPassword extends Target
+
+  case object GetResourcePolicy extends Target
 
   case object GetSecretValue extends Target
 
+  case object ListSecrets extends Target
+
+  case object ListSecretVersionIds extends Target
+
+  case object PutResourcePolicy extends Target
+
+  case object RotateSecret extends Target
+
   case object PutSecretValue extends Target
 
-  case object DescribeSecret extends Target
+  case object RestoreSecret extends Target
+
+  case object TagResource extends Target
+
+  case object UntagResource extends Target
+
+  case object UpdateSecret extends Target
+
+  case object UpdateSecretVersionStag extends Target
 
 }
