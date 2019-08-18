a7625dcac176130a03c8494bae00698e1acb600a
sali <sali@loyalty.com>
2019-08-17 21:36:56 -0400

    close repository upon shutdown

--- src/main/scala/com/alphasystem/aws/secretsmanager/Main.scala
+++ src/main/scala/com/alphasystem/aws/secretsmanager/Main.scala
@@ -1,5 +1,6 @@
 package com.alphasystem.aws.secretsmanager
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.server.{HttpApp, Route}
@@ -7,6 +8,8 @@ import akka.stream.{ActorMaterializer, Materializer}
 import com.alphasystem.aws.secretsmanager.repository.NitriteRepository
 import com.alphasystem.aws.secretsmanager.routes.Routes
 import com.typesafe.config.ConfigFactory
+
+import scala.util.Try
 
 object Main extends HttpApp with App with Routes {
 
@@ -20,4 +23,12 @@ object Main extends HttpApp with App with Routes {
   override protected def routes: Route = _routes
 
   Main.startServer(settings.http.host, settings.http.port, system)
+
+  override protected def postServerShutdown(attempt: Try[Done], system: ActorSystem): Unit = {
+    super.postServerShutdown(attempt, system)
+    attempt.toOption match {
+      case Some(_) => repository.close()
+      case None => // do nothing
+    }
+  }
 }
